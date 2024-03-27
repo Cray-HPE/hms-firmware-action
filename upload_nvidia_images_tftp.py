0b8558f4d71570fc7f9bb1ab61ebba422e09605e
Mike Buchmann <michael.buchmann@hpe.com>
2024-03-27 18:00:33 -0500

    Update to copyright date

--- upload_nvidia_images_tftp.py
+++ upload_nvidia_images_tftp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2021-2023 Hewlett Packard Enterprise Development LP
+# Copyright 2021-2024 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
